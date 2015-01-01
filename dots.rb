b86377539971da2a15ad894a3b1dca9db295fbeb
William Melody <hi@williammelody.com>
2015-01-01 12:40:23 -0800

    Add head option to dots formula

--- dots.rb
+++ dots.rb
@@ -4,6 +4,8 @@ class Dots < Formula
   homepage "https://github.com/alphabetum/dots"
   url "https://github.com/alphabetum/dots.git", :using => :git, :tag => "1.0.0"
 
+  head "https://github.com/alphabetum/dots.git"
+
   def install
     bin.install 'dots'
   end
