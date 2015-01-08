5685f7a95b69d8a7423f13c438db00c75e9c130c
William Melody <hi@williammelody.com>
2015-01-07 17:25:44 -0800

    Add swiftly head-only formula.

---
+++ swiftly.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Swiftly < Formula
+  homepage "https://github.com/alphabetum/swiftly"
+  head "https://github.com/alphabetum/swiftly.git"
+
+  def install
+    bin.install 'swiftly'
+  end
+
+  test do
+    system "#{bin}/swiftly"
+  end
+end
+
+
+
