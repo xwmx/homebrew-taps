b30e6df5e112bd39ebd6d6b88d976e8953af0dff
William Melody <hi@williammelody.com>
2015-01-15 18:31:00 -0800

    Bump dots version to 2.4.0

--- dots.rb
+++ dots.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Dots < Formula
   homepage "https://github.com/alphabetum/dots"
-  url "https://github.com/alphabetum/dots.git", :using => :git, :tag => "2.3.1"
+  url "https://github.com/alphabetum/dots.git", :using => :git, :tag => "2.4.0"
 
   head "https://github.com/alphabetum/dots.git"
 
