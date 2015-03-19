15baf97b75e3f77da74aed4ece271aedd85414d7
William Melody <hi@williammelody.com>
2015-03-18 18:35:18 -0700

    Add hosts tap @ 1.0.0

---
+++ hosts.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Hosts < Formula
+  homepage "https://github.com/alphabetum/hosts"
+  url "https://github.com/alphabetum/hosts.git", :using => :git, :tag => "1.0.0"
+
+  head "https://github.com/alphabetum/hosts.git"
+
+  def install
+    bin.install "hosts"
+  end
+
+  test do
+    system "#{bin}/hosts"
+  end
+end
+
+
