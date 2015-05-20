855ca9b2740ba1b57e603b39e773dc66f21be11d
William Melody <hi@williammelody.com>
2015-05-20 16:30:23 -0700

    Add bask v0.1.0

---
+++ bask.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Bask < Formula
+  homepage "https://github.com/alphabetum/bask"
+  url "https://github.com/alphabetum/bask.git", :using => :git, :tag => "0.1.0"
+
+  head "https://github.com/alphabetum/bask.git"
+
+  def install
+    bin.install 'bask'
+  end
+
+  test do
+    system "#{bin}/bask"
+  end
+end
+
+
