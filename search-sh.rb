0858969b1f4faee5295d3386db9d90e1aa0f1a70
William Melody <hi@williammelody.com>
2015-05-20 21:33:28 -0700

    Add search-sh formula

---
+++ search-sh.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class SearchSh < Formula
+  homepage "https://github.com/alphabetum/search.sh"
+  url "https://github.com/alphabetum/search.sh", :using => :git, :tag => "0.1.0"
+
+  head "https://github.com/alphabetum/search.sh.git"
+
+  def install
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/search"
+  end
+end
+
+
+
