032482160d1886356c8479e872b7a476839a1ac2
William Melody <hi@williammelody.com>
2015-05-23 10:52:39 -0700

    Add bindle formula

---
+++ bindle.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class Bindle < Formula
+  homepage "https://github.com/alphabetum/bindle"
+  url "https://github.com/alphabetum/bindle.git",
+    :using => :git,
+    :tag => "5.0.0"
+
+  head "https://github.com/alphabetum/bindle.git"
+
+  def install
+    bin.install "bindle"
+  end
+
+  test do
+    system "#{bin}/bindle"
+  end
+end
+
