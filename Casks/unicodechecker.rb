dfc7993b1df7febf10a6973b2b163acf3204c93b
William Melody <hi@williammelody.com>
2015-05-22 17:14:42 -0700

    Update unicodechecker hash

--- Casks/unicodechecker.rb
+++ Casks/unicodechecker.rb
@@ -1,10 +1,10 @@
 cask :v1 => "unicodechecker" do
   version :latest
-  sha256 "13f056d3af785fbb1764dda34e4bfddb15160032aa0df8e6286ab0557f1ad066"
+  sha256 "b5eb6668bd9c52c82d5f8aef8b062fafa482b1ae3e02d04ef50375c84bfc9c4a"
 
   url "http://earthlingsoft.net/UnicodeChecker/UnicodeChecker.zip"
   homepage "http://earthlingsoft.net/UnicodeChecker/"
   license :gratis
 
-  app "UnicodeChecker 1.16/UnicodeChecker.app"
+  app "UnicodeChecker 1.16.1/UnicodeChecker.app"
 end
