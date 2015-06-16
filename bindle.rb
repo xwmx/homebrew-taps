99ffe97a730bebc80750257fb871c5dc02ce02e0
William Melody <hi@williammelody.com>
2015-06-15 21:21:00 -0700

    Bump bindle to 5.0.3

--- bindle.rb
+++ bindle.rb
@@ -4,7 +4,7 @@ class Bindle < Formula
   homepage "https://github.com/alphabetum/bindle"
   url "https://github.com/alphabetum/bindle.git",
     :using => :git,
-    :tag => "5.0.2"
+    :tag => "5.0.3"
 
   head "https://github.com/alphabetum/bindle.git"
 
