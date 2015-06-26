f780ebb7feb795e34a355fc9611842bd2b052ab3
William Melody <hi@williammelody.com>
2015-06-25 23:14:29 -0700

    Add airport formula

---
+++ airport.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class Airport < Formula
+  homepage "https://github.com/alphabetum/airport"
+  url "https://github.com/alphabetum/airport.git",
+    :using => :git,
+    :tag => "0.1.0"
+
+  head "https://github.com/alphabetum/airport.git"
+
+  def install
+    bin.install "airport"
+  end
+
+  test do
+    system "#{bin}/airport"
+  end
+end
+
