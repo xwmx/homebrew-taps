1b1e2bb7e5737a9093801f0f6ef137d3993444cd
William Melody <hi@williammelody.com>
2015-09-26 18:25:44 -0700

    Add `accounts` tap at version 1.0.0

---
+++ accounts.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Accounts < Formula
+  homepage "https://github.com/alphabetum/accounts"
+  url "https://github.com/alphabetum/accounts.git",
+    :using => :git,
+    :tag => "1.0.0"
+
+  head "https://github.com/alphabetum/accounts.git"
+
+  def install
+    bin.install "accounts"
+  end
+
+  test do
+    system "#{bin}/accounts"
+  end
+end
