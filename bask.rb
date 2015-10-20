2f51ca5dc8c37f4d03a6a7b05b56d286c0e19f52
William Melody <hi@williammelody.com>
2015-10-19 17:12:53 -0700

    Bump bask version to 0.3.4.

--- bask.rb
+++ bask.rb
@@ -4,7 +4,7 @@ class Bask < Formula
   homepage "https://github.com/alphabetum/bask"
   url "https://github.com/alphabetum/bask.git",
     :using => :git,
-    :tag => "0.3.2"
+    :tag => "0.3.4"
 
   head "https://github.com/alphabetum/bask.git"
 
