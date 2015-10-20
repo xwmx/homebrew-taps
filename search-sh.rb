31a40e61847a5696f3796e9270a090bb3b191c64
William Melody <hi@williammelody.com>
2015-10-19 17:22:14 -0700

    Bump search.sh version to 0.1.2.

--- search-sh.rb
+++ search-sh.rb
@@ -4,7 +4,7 @@ class SearchSh < Formula
   homepage "https://github.com/alphabetum/search.sh"
   url "https://github.com/alphabetum/search.sh",
     :using => :git,
-    :tag => "0.1.1"
+    :tag => "0.1.2"
 
   head "https://github.com/alphabetum/search.sh.git"
 
