b4727c3811abed7af14c36c7de0b875b0b92f291
William Melody <hi@williammelody.com>
2015-11-09 17:57:31 -0800

    Add pb formula @ 1.0.0

---
+++ pb.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Pb < Formula
+  homepage "https://github.com/alphabetum/pb"
+  url "https://github.com/alphabetum/pb.git",
+    :using => :git,
+    :tag => "1.0.0"
+
+  head "https://github.com/alphabetum/pb.git"
+
+  def install
+    bin.install "pb"
+  end
+
+  test do
+    system "#{bin}/pb"
+  end
+end
