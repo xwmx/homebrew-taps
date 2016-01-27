2db838268215e8e0b088a00383b50992320be278
William Melody <hi@williammelody.com>
2016-01-26 20:45:15 -0800

    Bump hosts version to 2.1.5.

--- hosts.rb
+++ hosts.rb
@@ -4,7 +4,7 @@ class Hosts < Formula
   homepage "https://github.com/alphabetum/hosts"
   url "https://github.com/alphabetum/hosts.git",
     :using => :git,
-    :tag => "2.1.4"
+    :tag => "2.1.5"
 
   head "https://github.com/alphabetum/hosts.git"
 
