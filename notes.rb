2bafd01a522500831e9fd0bb7c7ea69da095de3f
William Melody <hi@williammelody.com>
2016-03-21 22:03:37 -0700

    Add notes formula at 1.0.0.

---
+++ notes.rb
@@ -0,0 +1,20 @@
+require "formula"
+
+class Notes < Formula
+  homepage "https://github.com/alphabetum/notes"
+  url "https://github.com/alphabetum/notes.git",
+    :using => :git,
+    :tag => "1.0.0"
+
+  head "https://github.com/alphabetum/notes.git"
+
+  def install
+    bin.install "notes"
+    zsh_completion.install "notes.autocomplete.zsh"
+    bash_completion.install "notes.autocomplete.bash"
+  end
+
+  test do
+    system "#{bin}/notes"
+  end
+end
