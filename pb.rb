704d6463d4ef2243eb5a49236ec4570486201cb6
William Melody <hi@williammelody.com>
2016-03-24 20:21:59 -0700

    Bump pb version to 1.0.3.

--- pb.rb
+++ pb.rb
@@ -4,7 +4,7 @@ class Pb < Formula
   homepage "https://github.com/alphabetum/pb"
   url "https://github.com/alphabetum/pb.git",
     :using => :git,
-    :tag => "1.0.2"
+    :tag => "1.0.3"
 
   head "https://github.com/alphabetum/pb.git"
 
