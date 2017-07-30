b0c593e77347ba05207fcf8afda19f56e2b12efc
{ɑβ} <hi@williammelody.com>
2017-07-29 20:13:20 -0700

    Bump notes to 1.0.4

--- notes.rb
+++ notes.rb
@@ -4,7 +4,7 @@ class Notes < Formula
   homepage "https://github.com/alphabetum/notes"
   url "https://github.com/alphabetum/notes.git",
     :using => :git,
-    :tag => "1.0.3"
+    :tag => "1.0.4"
 
   head "https://github.com/alphabetum/notes.git"
 
