63a2202aea35763383ee380a2109833bd1b9be63
William Melody <hi@williammelody.com>
2018-05-09 10:34:42 -0700

    Update accounts to 1.2.0

--- accounts.rb
+++ accounts.rb
@@ -4,7 +4,7 @@ class Accounts < Formula
   homepage "https://github.com/alphabetum/accounts"
   url "https://github.com/alphabetum/accounts.git",
     :using => :git,
-    :tag => "1.1.3"
+    :tag => "1.2.0"
 
   head "https://github.com/alphabetum/accounts.git"
 
