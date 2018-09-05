f1e50fc50b4b31af43bb417c493277a3374bde04
William Melody <hi@williammelody.com>
2018-09-05 13:29:17 -0700

    Update airport to 0.4.0

--- airport.rb
+++ airport.rb
@@ -4,7 +4,7 @@ class Airport < Formula
   homepage "https://github.com/alphabetum/airport"
   url "https://github.com/alphabetum/airport.git",
     :using => :git,
-    :tag => "0.3.1"
+    :tag => "0.4.0"
 
   head "https://github.com/alphabetum/airport.git"
 
