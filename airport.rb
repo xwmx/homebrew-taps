7036b4d053c78dcfad22429a291833bd3d855079
William Melody <hi@williammelody.com>
2018-09-05 13:38:16 -0700

    Bump airport to 0.4.1

--- airport.rb
+++ airport.rb
@@ -4,7 +4,7 @@ class Airport < Formula
   homepage "https://github.com/alphabetum/airport"
   url "https://github.com/alphabetum/airport.git",
     :using => :git,
-    :tag => "0.4.0"
+    :tag => "0.4.1"
 
   head "https://github.com/alphabetum/airport.git"
 
