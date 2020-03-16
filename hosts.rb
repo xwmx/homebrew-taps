5e3e4a601fbb512b86174990bc6844081bee8fa0
William Melody <hi@williammelody.com>
2020-03-15 17:09:40 -0700

    Update hosts to 3.1.3

--- hosts.rb
+++ hosts.rb
@@ -4,7 +4,7 @@ class Hosts < Formula
   homepage "https://github.com/alphabetum/hosts"
   url "https://github.com/alphabetum/hosts.git",
     :using => :git,
-    :tag => "3.1.2"
+    :tag => "3.1.3"
 
   head "https://github.com/alphabetum/hosts.git"
 
