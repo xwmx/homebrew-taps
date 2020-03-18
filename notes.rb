de183fae37ebc70c37487f9671e6ed779aaa178b
William Melody <hi@williammelody.com>
2020-03-18 12:24:37 -0700

    Update notes to 1.1.2

--- notes.rb
+++ notes.rb
@@ -4,7 +4,7 @@ class Notes < Formula
   homepage "https://github.com/xwmx/notes"
   url "https://github.com/xwmx/notes.git",
     :using => :git,
-    :tag => "1.1.1"
+    :tag => "1.1.2"
 
   head "https://github.com/xwmx/notes.git"
 
