f4baced3b778fdabf99bf3c2d625f6416ed18ac5
William Melody <hi@williammelody.com>
2020-03-23 11:04:41 -0700

    Rename 'accounts' to 'user' and update to 2.0.0

---
+++ user.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Accounts < Formula
+  homepage "https://github.com/xwmx/user"
+  url "https://github.com/xwmx/user.git",
+    :using => :git,
+    :tag => "2.0.0"
+
+  head "https://github.com/xwmx/user.git"
+
+  def install
+    bin.install "user"
+  end
+
+  test do
+    system "#{bin}/user"
+  end
+end
