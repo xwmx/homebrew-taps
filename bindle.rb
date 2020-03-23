015bfd8d2d25250858ee9face4ee0d911bdece47
William Melody <hi@williammelody.com>
2020-03-23 10:15:52 -0700

    Update bindle to 7.0.3

--- bindle.rb
+++ bindle.rb
@@ -4,7 +4,7 @@ class Bindle < Formula
   homepage "https://github.com/xwmx/bindle"
   url "https://github.com/xwmx/bindle.git",
     :using => :git,
-    :tag => "6.0.2"
+    :tag => "7.0.3"
 
   head "https://github.com/xwmx/bindle.git"
 
