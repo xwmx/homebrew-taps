9673e222a9f970baa849eaf5b0cdb63d0802d70d
William Melody <hi@williammelody.com>
2020-03-30 17:47:09 -0700

    Update hosts to 3.2.5

--- hosts.rb
+++ hosts.rb
@@ -4,7 +4,7 @@ class Hosts < Formula
   homepage "https://github.com/xwmx/hosts"
   url "https://github.com/xwmx/hosts.git",
     :using => :git,
-    :tag => "3.2.4"
+    :tag => "3.2.5"
 
   head "https://github.com/xwmx/hosts.git"
 
