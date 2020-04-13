9befc2232df4d5bae1648a24cf33938027f8a728
William Melody <hi@williammelody.com>
2020-04-13 15:33:40 -0700

    Update hosts to 3.3.2 and improve completion.

--- hosts.rb
+++ hosts.rb
@@ -4,12 +4,14 @@ class Hosts < Formula
   homepage "https://github.com/xwmx/hosts"
   url "https://github.com/xwmx/hosts.git",
     :using => :git,
-    :tag => "3.3.1"
+    :tag => "3.3.2"
 
   head "https://github.com/xwmx/hosts.git"
 
   def install
     bin.install "hosts"
+    zsh_completion.install "hosts-completion.zsh" => "_hosts"
+    bash_completion.install "hosts-completion.bash"
   end
 
   test do
