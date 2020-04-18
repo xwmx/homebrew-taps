11d65608962108923474a1a5b2ae36b3ed18b9f9
William Melody <hi@williammelody.com>
2020-04-17 22:50:26 -0700

    Update notes to 2.14.0

--- notes.rb
+++ notes.rb
@@ -4,12 +4,13 @@ class Notes < Formula
   homepage "https://github.com/xwmx/notes"
   url "https://github.com/xwmx/notes.git",
     :using => :git,
-    :tag => "2.12.12"
+    :tag => "2.14.0"
 
   head "https://github.com/xwmx/notes.git"
 
   def install
     bin.install "notes"
+    bin.install "bin/bookmark"
     zsh_completion.install "etc/notes-completion.zsh" => "_notes"
     bash_completion.install "etc/notes-completion.bash" => "notes"
   end
