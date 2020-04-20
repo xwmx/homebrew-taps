249ad4ffafa0636b3bbdffce3b47612632accedd
William Melody <hi@williammelody.com>
2020-04-19 17:30:08 -0700

    Update dependencies in notes.rb

--- notes.rb
+++ notes.rb
@@ -8,6 +8,12 @@ class Notes < Formula
 
   head "https://github.com/xwmx/notes.git"
 
+  depends_on "pandoc"
+  depends_on "pygments"
+  depends_on "ripgrep"
+  depends_on "tig"
+  depends_on "w3m"
+
   def install
     bin.install "notes"
     bin.install "bin/bookmark"
