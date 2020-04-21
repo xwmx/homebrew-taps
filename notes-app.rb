3985a09a3f5953479dd612f8da627a1af49b168d
William Melody <hi@williammelody.com>
2020-04-20 23:30:26 -0700

    Update notes-app to 0.0.4

--- notes-app.rb
+++ notes-app.rb
@@ -4,7 +4,7 @@ class NotesApp < Formula
   homepage "https://github.com/xwmx/notes-app-cli"
   url "https://github.com/xwmx/notes-app-cli.git",
     :using => :git,
-    :tag => "0.0.3"
+    :tag => "0.0.4"
 
   head "https://github.com/xwmx/notes-app-cli.git"
 
