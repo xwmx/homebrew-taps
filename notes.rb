22fb5a1b152f854aac4ebc999fd5a15649facf2b
William Melody <hi@williammelody.com>
2020-04-27 06:20:06 -0700

    Update notes to 2.16.6

--- notes.rb
+++ notes.rb
@@ -4,7 +4,7 @@ class Notes < Formula
   homepage "https://github.com/xwmx/notes"
   url "https://github.com/xwmx/notes.git",
     :using => :git,
-    :tag => "2.16.5"
+    :tag => "2.16.6"
 
   head "https://github.com/xwmx/notes.git"
 
