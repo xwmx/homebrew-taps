c219a5a1a425208fab8b0911d6a86244e63b7c7f
William Melody <hi@williammelody.com>
2020-05-12 12:04:31 -0700

    Rename notes to nb.

---
+++ nb.rb
@@ -0,0 +1,29 @@
+require "formula"
+
+class Nb < Formula
+  homepage "https://github.com/xwmx/nb"
+  url "https://github.com/xwmx/nb.git",
+    :using => :git,
+    :tag => "4.0.0"
+
+  head "https://github.com/xwmx/nb.git"
+
+  depends_on "git"
+  depends_on "pandoc"
+  depends_on "pygments"
+  depends_on "ripgrep"
+  depends_on "tig"
+  depends_on "w3m"
+
+  def install
+    bin.install "nb"
+    bin.install "bin/notes"
+    bin.install "bin/bookmark"
+    zsh_completion.install "etc/nb-completion.zsh" => "_nb"
+    bash_completion.install "etc/nb-completion.bash" => "nb"
+  end
+
+  test do
+    system "#{bin}/nb"
+  end
+end
