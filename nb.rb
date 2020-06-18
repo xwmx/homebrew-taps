5d365e662b51a601c3c23ce40bb1afa2d8450fbb
William Melody <hi@williammelody.com>
2020-06-17 19:15:59 -0700

    Update nb to 4.1.22

--- nb.rb
+++ nb.rb
@@ -4,7 +4,7 @@ class Nb < Formula
   homepage "https://github.com/xwmx/nb"
   url "https://github.com/xwmx/nb.git",
     :using => :git,
-    :tag => "4.1.21"
+    :tag => "4.1.22"
 
   head "https://github.com/xwmx/nb.git"
 
