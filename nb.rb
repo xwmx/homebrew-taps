e67a68c956acbb73672dfffa1f30c52b24f2a00e
William Melody <hi@williammelody.com>
2020-09-14 23:03:57 -0700

    Update nb to 4.7.4

--- nb.rb
+++ nb.rb
@@ -4,7 +4,7 @@ class Nb < Formula
   homepage "https://github.com/xwmx/nb"
   url "https://github.com/xwmx/nb.git",
     :using => :git,
-    :tag => "4.7.3"
+    :tag => "4.7.4"
 
   head "https://github.com/xwmx/nb.git"
 
