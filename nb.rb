5dc746bb2656d61710749297760106b24ff197f9
William Melody <hi@williammelody.com>
2020-10-10 23:52:05 -0700

    Update nb to 5.5.9

--- nb.rb
+++ nb.rb
@@ -4,7 +4,7 @@ class Nb < Formula
   homepage "https://github.com/xwmx/nb"
   url "https://github.com/xwmx/nb.git",
     :using => :git,
-    :tag => "5.5.8"
+    :tag => "5.5.9"
 
   head "https://github.com/xwmx/nb.git"
 
