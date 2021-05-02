c28c9de2002ae273343a4600db4423709d95aaca
William Melody <hi@williammelody.com>
2021-05-02 12:43:13 -0700

    Update nb to 6.1.3.

--- nb.rb
+++ nb.rb
@@ -4,7 +4,7 @@ class Nb < Formula
   homepage "https://github.com/xwmx/nb"
   url "https://github.com/xwmx/nb.git",
     :using  => :git,
-    :tag    => "6.1.2"
+    :tag    => "6.1.3"
 
   head "https://github.com/xwmx/nb.git"
 
