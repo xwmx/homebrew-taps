04c521c359b895ff86c4b3acb5d21cedbcd69159
William Melody <hi@williammelody.com>
2023-07-21 10:50:52 -0700

    Update `nb` to 7.5.5.

--- nb.rb
+++ nb.rb
@@ -4,7 +4,7 @@ class Nb < Formula
   homepage "https://github.com/xwmx/nb"
   url "https://github.com/xwmx/nb.git",
       using:  :git,
-      tag:    "7.5.4"
+      tag:    "7.5.5"
 
   head "https://github.com/xwmx/nb.git"
 
