186c514ecbedbc31ae01c5b7a7d8d5cc646b38a9
William Melody <hi@williammelody.com>
2026-03-09 15:13:59 -0700

    Update `pb` to 1.3.2.

--- pb.rb
+++ pb.rb
@@ -4,7 +4,7 @@ class Pb < Formula
   homepage "https://github.com/xwmx/pb"
   url "https://github.com/xwmx/pb.git",
     :using  => :git,
-    :tag    => "1.3.1"
+    :tag    => "1.3.2"
 
   head "https://github.com/xwmx/pb.git"
 
